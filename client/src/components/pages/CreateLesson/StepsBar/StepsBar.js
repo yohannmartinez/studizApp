c731d794d02c8b6704d4bd3f0e2497670b7cab1a
yohann martinez <yohann.martinez@goshaba.com>
2021-11-25 22:40:46 +0100

    added step colors

--- client/src/components/pages/CreateLesson/StepsBar/StepsBar.js
+++ client/src/components/pages/CreateLesson/StepsBar/StepsBar.js
@@ -2,6 +2,17 @@ import "./StepsBar.scss";
 
 const StepsBar = ({ currentStep }) => {
   const steps = ["La base", "Les filtres", "Cours créé"];
+
+  const stepColor = (stepIndex) => {
+    const doneStepColor = "#36b336";
+    const currentStepColor = "#000000";
+    const otherStepColor = "#bebebe";
+
+    if (currentStep === stepIndex) return currentStepColor;
+    if (stepIndex < currentStep) return doneStepColor;
+    if (currentStep < stepIndex) return otherStepColor;
+  };
+
   return (
     <div className="stepsBar__stepsContainer">
       {steps.map((step, index) => (
@@ -9,14 +20,14 @@ const StepsBar = ({ currentStep }) => {
           <div
             className="stepsBar__step"
             style={{
-              borderColor: currentStep > index ? "#36b336" : "black",
-              color: currentStep > index ? "#36b336" : "black",
+              borderColor: stepColor(index),
+              color: stepColor(index),
             }}
           >
             {index + 1}
             <div
               className="stepsBar__stepName"
-              style={{ color: currentStep > index ? "#36b336" : "black" }}
+              style={{ color: stepColor(index) }}
             >
               {step}
             </div>
